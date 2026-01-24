9ea21429592cf5ed58d174024d7ef02a16d1077c
Christina Vahi <christina.vahi@tptlive.ee>
2026-01-24 12:09:42 +0200

    Add type examples

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
-echo 'Hello World';
+// echo 'Hello World';
+
+// kõik muutujad algavad $ märgiga ja semikoolon ; on oluline plokkide lõpus
+
+$num = 10;
+$num =10.2;
+$name = 'Christina';
+// ühekordsed jutumärgid - sellevaheline esitatakse alati tekstina
+$num = 'Hello World';
+// siin on oluline vahe, kas "" või ''. Topeltjutumärgid oskavad tõlgendada reavahemärki ja muutujaid
+$num = "Hello \n$name";
+// kui vaja multiline teksti
+$num = <<<END
+Hello
+$name
+END;
+$num = true;
+$num = false;
+$num = null;
+// on olemas array-d, on olemas kaks viisi, vanem viis
+$num = array(1,2,3,3,4);
+// uuem array viis kandilised sulud
+$num = [1,2,3,4];
+// objektid, default objekt stdClass, std tähendab standard
+$num = new stdClass();
+var_dump ($num);
+// kui toob välja string (12), siis  näitab baitide, mitte tähemärkide arvu, nt õ puhul 2 baiti
+
+
+
 // class Box {
 //     public $width;
 //     public $height;
