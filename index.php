5f19b31d06d68210e7e85e1595dd30c747ab88ce
Christina Vahi <christina.vahi@tptlive.ee>
2026-01-24 13:23:55 +0200

    Add round fix as well

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $num =  1 + 2;
 $num =  0.1 + 0.2; // php annab tulemuseks float(0.30000000000000004)
 // see tuleneb sellest ,et kõiki komaarve ei saa kahendsüsteemis täpselt kirjutada
 // sellepärast nt pangaandmesüsteemides on summad sentides, mitte eurodes
+
+// üks võimalus, kuidas lahendada
+$num = round (0.1 + 0.2, 1);
+$num = round (0.1*10 + 0.2*10)/10;
 var_dump($num);
 
 
