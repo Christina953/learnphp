c4a1687c1482d3b405db12525364c959feb91225
Christina Vahi <christina.vahi@tptlive.ee>
2026-01-24 20:39:51 +0200

    Add classes and objects

--- index.php
+++ index.php
@@ -1,63 +1,35 @@
 <?php
 
-// funktsioonid, eesmärk on mingit koodi plokki korrata, et funktsioon teeks midagi, peab selle välja kutsuma
+// klassid, kokkulepe klasside nimed suure tähega
+// klass määrab omadused + tegevus
+// klass ei ole objekt
 
-function hello() {
-    var_dump('hello');
+
+class Box {
+    public $width;
+    public $height;
+    public $length;
+
+    public function volume() {
+        return $this->width * $this->height * $this->length; // kui this ees $ siis peale noolt enamasit $ pole vaja
+    } // $this on eriline muutuja, mis viitab praegusele objektile.
+      //Teisisõnu: see “mina” objekt, mille sees see meetod töötab.
+      //Kui oleks ainult width → PHP ei tea, millise objekti width sa mõtled
+      // $this ütleb: “kasuta just seda objekti, millelt meetod kutsuti”
 }
 
-hello();
+$box1 = new Box();
+$box1->width = 10;
+$box1->height = 10;
+$box1->length = 10;
+var_dump($box1->volume());
+var_dump($box1);
 
-function helloName($name='Nameless') { // võib anda default väärtuse, kui muutuja jääb tühjaks
-    var_dump("Hello $name!");
-}
-
-helloName('Christina');
-helloName('Margo');
-
-function sum($a, $b) {
-    return $a+$b; // kui midagi lisada peale returni, siis seda ei toimu
-}
-$answer = sum(2, 4);
-var_dump($answer);
-
-function values(...$values) { // ... ükskõik mis arv väärtusi
-    var_dump(...$values); // kui lisada siia ka ..., siis kirjutab numbrid eraldi välja, st nimet. spread operator
-    // PHP-s on spread operator tähistatud kolme punktiga: ...See “laotab” massiivi või objektide kogumi eraldi elementideks
-}
- values(1,2,3,4,5,6);
-
-function name(string $name):string { //kui tahame et tagatus oleks ka string
-    return ucwords($name);
-}
-// Mis on recursive (rekursiivne) funktsioon? Funktsioon, mis kutsub iseennast.
-// Kasutatakse, kui probleem saab lahendatud, jagades selle väiksemateks sama tüüpi probleemideks.
-// Seega iga rekursiooni puhul peab olma tingimus, mis lõpuks muutub false’iks
-// kasutatakse olukordades recursive trees 
-function recursive($i) {
-    var_dump($i);
-    if($i < 10){
-        recursive($i+1);
-    }
-}
-
-recursive(0);
-
-
-// class Box {
-//     public $width;
-//     public $height;
-//     public $length;
-
-//     public function volume() {
-//         return $this->width * $this->height * $this->length;
-//     }
-// }
-
-// $box1 = new Box();
-// $box1->width = 10;
-// $box1->height = 10;
-// $box1->length = 10;
-// var_dump($box1->volume());
-// var_dump($box1);
+$box2 = new Box();
+$box2->width = 20;
+$box2->height = 20;
+$box2->length = 20;
+var_dump($box2->volume());
+var_dump($box2);
+var_dump($box1);
 
