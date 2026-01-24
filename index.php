ee78d163790c5640ac8a5443ac7e44795e1b93ae
Christina Vahi <christina.vahi@tptlive.ee>
2026-01-24 20:27:00 +0200

    add function examples

--- index.php
+++ index.php
@@ -1,47 +1,48 @@
 <?php
 
-// for loop - for tsüklis kolm osa, eraldatakse semikooloniga
-// esimeses osas luuakse loendur, $i on muutuja (tihti kasutatakse just i, sest index) ja sellele antakse algväärtus
-// paneb paika, kui kaua seda kontrolli tehakse, Tsükkel töötab seni, kuni see on tõene
-// samm / muutmine, See juhtub pärast iga tsükli ringi, $i++ tähendab: suurenda $i väärtust ühe võrra
+// funktsioonid, eesmärk on mingit koodi plokki korrata, et funktsioon teeks midagi, peab selle välja kutsuma
 
-for($i=0; $i<10 ; $i++) {
+function hello() {
+    var_dump('hello');
+}
+
+hello();
+
+function helloName($name='Nameless') { // võib anda default väärtuse, kui muutuja jääb tühjaks
+    var_dump("Hello $name!");
+}
+
+helloName('Christina');
+helloName('Margo');
+
+function sum($a, $b) {
+    return $a+$b; // kui midagi lisada peale returni, siis seda ei toimu
+}
+$answer = sum(2, 4);
+var_dump($answer);
+
+function values(...$values) { // ... ükskõik mis arv väärtusi
+    var_dump(...$values); // kui lisada siia ka ..., siis kirjutab numbrid eraldi välja, st nimet. spread operator
+    // PHP-s on spread operator tähistatud kolme punktiga: ...See “laotab” massiivi või objektide kogumi eraldi elementideks
+}
+ values(1,2,3,4,5,6);
+
+function name(string $name):string { //kui tahame et tagatus oleks ka string
+    return ucwords($name);
+}
+// Mis on recursive (rekursiivne) funktsioon? Funktsioon, mis kutsub iseennast.
+// Kasutatakse, kui probleem saab lahendatud, jagades selle väiksemateks sama tüüpi probleemideks.
+// Seega iga rekursiooni puhul peab olma tingimus, mis lõpuks muutub false’iks
+// kasutatakse olukordades recursive trees 
+function recursive($i) {
     var_dump($i);
-}
-// sama asja võib teha ka teistmoodi, nii käib tagurpidi
-for($i=10; $i>0 ; $i--) {
-    var_dump($i);
-}
-// _ võib kasutada suurte numbrite puhul tuhandete eraldajana
-for($i=8; $i<10_000_000 ; $i*=2) {
-    var_dump($i);
+    if($i < 10){
+        recursive($i+1);
+    }
 }
 
-//while loop - siin ei tea mitu korda alati
-$time = time(); // tagastab Unix timestamp
-var_dump($time);
-$i = 0;
-while($time+3 > time()){ //Tee tsüklit nii kaua, kuni algusaeg + 3 sekundit on suurem kui praegune aeg
-    $i++; //Iga tsükli ring: $i suureneb 1 võrra, Midagi muud ei juhtu, tsükkel jookseb nii kiiresti kui CPU suudab
-}
-var_dump($i); // Näitab, mitu korda tsükkel 3 sekundi jooksul käis, erinev igal käivitamisel, sõltub arvuti kiirusest
+recursive(0);
 
-// do while kood käivitab tsükli sisu täpselt ühe korra,
-// käivitab sisu kõigepealt ja alles pärast seda kontrollib tingimust
-do {
-    var_dump('DO');
-} while(false);
-
-$array = ['apple', 'cherry', 'pear'];
-foreach($array as $value) {  //foreach käib iga massiivi elemendi läbi, ükshaaval. $value on ajutine muutuja
-// , igal tsükli ringil saab ta järgmise elemendi väärtuse, kirjutab rea haaval välja
-    var_dump($value);
-}
-
-$array = ['apple', 'cherry', 'pear'];
-foreach($array as $key => $value) { // $key on massiivi võtme väärtus ehk indeks, mille all see element massiivis asub
-    var_dump($key, $value);
-}
 
 // class Box {
 //     public $width;
