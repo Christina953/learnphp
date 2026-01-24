cc1349148af37d6dae2e7e552b2c1cf32cbcb3ba
Christina Vahi <christina.vahi@tptlive.ee>
2026-01-24 18:55:37 +0200

    Add if and switch examples

--- index.php
+++ index.php
@@ -1,68 +1,62 @@
 <?php
-//massiivid
-$array = [1 , 2, 3];
-var_dump($array[1]);
-//kuidas lisada massiivi, array [] = 4 lisab array lõppu selle väärtuse
-$array[] = 4;
-// saab array-s olevat arvu muuta
-$array[2] = 22;
-$array = [
-    'name' => 'Christina',
-    'age' => 50,
-    ];
-//See on võti ja väärtus (key => value)
-//Seega massiiv sidub võtme "name" väärtusega "Christina"
 
-// võib ka miksida
-$array = [
-    1,
-    'hello' => 'world',
-    2,
-    3,
-    'cool' => 'stuff',
-    '4',
-    true,
-    [1, 2, 3],
-    ];
-// kui array sisaldab omakorda array'd, siis neid nimetatakse mitmemõõtmeliseks array'ks
-// kui tahad teise array väärtust kätte saada, siis nii:
-var_dump($array[5],[1]);
-$array = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-];
-var_dump($array[1][2]);
-$array = [
-    [[1,2,3],
-    [4,5,6],
-    [7,8,9],[1,2,3],
-    [4,5,6],
-    [7,8,9],[1,2,3],
-    [4,5,6],
-    [7,8,9],],
-    [[1,2,3],
-    [4,5,6],
-    [7,8,9],[1,2,3],
-    [4,5,6],
-    [7,8,9],[1,2,3],
-    [4,5,6],
-    [7,8,9],],
-    [[1,2,3],
-    [4,5,6],
-    [7,8,9],[1,2,3],
-    [4,5,6],
-    [7,8,9],[1,2,3],
-    [4,5,6],
-    [7,8,9]],
-];
-var_dump($array[2][2][2]);
-$array = ['apple', 'cherry','banana'];
-$text = implode(',',$array); // see muudab nimekirja väärtustest teksti
-// explode töötab vastupidi, muudab teksti array'ks
-var_dump($text);
+//kontrollstruktuurid
+ $num = 10;
+ if ($num > 10){
+    var_dump('Bigger than 10');
+} else if ($num === 10) {
+    var_dump('Equal to 10');
+} else {
+    var_dump('Smaller than 10');
+}
 
+$day = (int)date( 'N' , strtotime('25.01.2026')); // date ('N) annab, mitmes nädalapäev täna on
+// strtotime() muudab teksti kujul kuupäeva ajaks (timestamp’iks)
+// Tulemus on arv, millest PHP saab aru, et see on kindel kuupäev
+//Mõtle: “tee tekstist kuupäev, millega arvuti oskab töötada”
+//date('N', ...) - date() vormindab kuupäeva, 'N' tähendab: nädalapäeva number
 
+var_dump($day);
+
+if($day === 1){
+    var_dump('Monday');
+} else if ($day === 2) {
+    var_dump('Tuesday');
+} else if ($day === 3) {
+    var_dump('Wednesday');
+} else if ($day === 4) {
+    var_dump('Thursday');
+} else if ($day === 5) {
+    var_dump('Friday');
+} else if ($day === 6 || $day === 7) {
+    var_dump('It is weekend!');
+} else {
+    var_dump('Weird day');
+}
+
+switch($day) {
+    case 1:
+       var_dump('Monday');
+       break; // loeb nii kaua kui jõuab break'ini
+    case 2:
+        var_dump('Tuesday');
+        break;
+    case 3:
+       var_dump('Wednesday');
+       break;
+    case 4:
+       var_dump('Thursday');
+       break;
+    case 5:
+       var_dump('Friday');
+       break;
+    case 6:
+    case 7:
+       var_dump('It is weekend!');
+       break;
+    default:
+        var_dump('Weird day');
+}
 
 // class Box {
 //     public $width;
