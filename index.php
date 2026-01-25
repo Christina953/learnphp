4eb5a4b9145b09e2e6deff06ea413ccbad8fc666
Christina Vahi <christina.vahi@tptlive.ee>
2026-01-25 20:26:15 +0200

    Add magic method examples

--- index.php
+++ index.php
@@ -1,23 +1,55 @@
 <?php
 
-// klassid, kokkulepe klasside nimed suure tähega
-// klass määrab omadused + tegevus
-// klass ei ole objekt
-// funktsioonid on tihti public, aga omadused private
-
 class Box {
-    public $width; // “Seda omadust või meetodit võib kasutada igaüks, igalt poolt.”
-    // protected $width; saab kasutada selles klassis ja tema alamklassides
-    // private $width; saab kasutada ainult selles samas klassis
+    public int $width;
     public $height;
     public $length;
 
-    // Miks üldse peita omadusi? 1) hoiab andmed kontrolli all 2)väldib valesid väärtusi (nt laius = -100)
-    // 3) koodi on hiljem lihtsam muuta. Kuidas vältida neg väärtust publicu puhul?
+    // magic methods
+    public function __construct($width, $height,$length) // see võimaldab muutujad lisada kohe new MetalBox(10,10,10)
+    {
+        $this->width = $width;
+        $this->height = $height;
+        $this->length = $length;
+        var_dump('Object created');
+        // see on eriline funktsioon, mis käivitub automaatselt, kui objekt luuakse (nt New Metal Box)
+        //seda kasutatakse: algväärtuste seadmiseks, kohustuslike andmete küsimiseks, objekti “valmis seisu” panemiseks
+        // Konstruktor = kasutusjuhend koodis
+    }
+    // see on eriline meetod, mis käivitub automaatselt, kui objekt hävitatakse
+    // Milleks destruktorit päriselt kasutatakse? 1) Ressursside vabastamiseks (faili sulgemine, andmebaasiühenduse
+    // lõpetamine, lukustuste vabastamine) 2) Puhastustöö (cleanup), 3) Debugimiseks (õppimisel)
+    // Kokkuvõte: __destruct() kasutatakse ressursside vabastamiseks ja puhastamiseks, mitte äriloogika jaoks.
+
+    public function __destruct(){
+        var_dump('Object destroyed');
+    }
+    
+    // echo saab näidata teksti, ehk kasutatakse selleks, et öelda php-le
+    // kuidas seda objekti tekstina (stringina) näidata? Kus seda päriselt kasutatakse? 1) echo $object
+    // 2) print $object; 3) logides; 4) debugimisel 5)HTML-i kuvamisel . Ehk teeb objekti inimloetavaks, mugavalt prinditavaks
+    public function __toString() {
+        return 'some box';
+    }
+
+    // Käivitatakse automaatselt, kui: proovid lugeda omadust mida ei ole olemas või on private / protected
+    // võimaldab kontrollitud ligipääsu peidetud või virtuaalsetele omadustele
+    public function __get($name){
+        var_dump($name);
+        return 'some value';
+    }
+
+    // __set() on maagiline meetod, mis käivitatakse automaatselt, kui proovid omadusele väärtust anda aga see omadus
+    // ei eksisteeri või on private / protected. Kasutatakse: 1) Andmete kontroll (validation), ei lase valesid väärtusi sisse.
+    // 2) Virtuaalsed omadused; 3) Debugimine (õppimisel), näed kohe, mis toimub.
+    //võimaldab kontrollida ja töödelda omadustele väärtuse panemist, kui sellele ei pääse otse ligi. 
+    public function __set($name, $value){
+        var_dump($name, $value);
+    }
 
     public function setLength(int $length) {
         if($length<0){
-            throw new Exception('Add positive value'); // throw ja return lõpetab, siin else pole vaja
+            throw new Exception('Add positive value');
         }
         $this->length = $length;
     }
@@ -33,8 +65,15 @@ class MetalBox extends Box {
         return $this ->volume() * $this-> weightPerUnit;
     }
 }
-$metal1 = new MetalBox();
-$metal1->width = 10;
-$metal1->height = 10;
-$metal1->setLength(10);
+function makeBox() { // toimib funktsiooni sees, aga hävitatakse funktsiooni lõppedes
+    $metal1 = new MetalBox(10,10,10);
+    var_dump($metal1);
+}
+makeBox();
+// unset($metal1); // tühjendab ära muutujad, kas selle puhul objekt hävitatakse
+$metal1 = new MetalBox(10,10,10);
 var_dump($metal1);
+echo $metal1;
+var_dump($metal1->coolbeans);
+$metal1->catlitter = 'asdada';
+var_dump('end');
