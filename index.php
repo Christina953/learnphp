d289dbab78e89978ed064bb68aa4221b1ecee7be
Christina Vahi <christina.vahi@tptlive.ee>
2026-01-25 22:08:44 +0200

    Add static examples

--- index.php
+++ index.php
@@ -1,34 +1,35 @@
 <?php
-
+// klassi staatilised omadused, tegevused
+// static tähendab: see kuulub KLASSILE, mitte objektile. Staatilises meetodis EI OLE $this
+// Staatiline omadus või meetod on seotud klassiga, mitte objektiga, ja seda kasutatakse klassi nime kaudu
 class Box {
-    public $num;
+    public static $count;
+
+    public static function getCount(){ // Seda kutsutakse ilma objektita // staatilises funktsioonis ei saa $this välja kutsuda
+        var_dump(self::class); // self viitab classile Box
+        var_dump(self::$count);
+        var_dump(static::class); // static viitab sellele klassile kes ta välja kutsus
+        var_dump(static::$count);
+    }
+    public function getWidth(){// siin saab static kasutada, sest see on olemas kui klass defineeritud
+        var_dump(self::class); 
+        var_dump(self::$count);
+        var_dump(static::class); 
+        var_dump(static::$count);
+        var_dump($this->width);
+    }
 }
 
-$num1 = 1;
-$num2 = &$num1; // & tähendab: “viita samale mälukohale”, $num1 ja $num2 on üks ja sama asi, lihtsalt kaks nime
-$num2 = 2;
-var_dump($num1, $num2);
+class MetalBox extends Box{
 
-// PHP-s on objektid vaikimisi by reference-like, $box1 ja $box2 ei hoia objekti ennast, nad hoiavad viidet objektile
-// $box2 viitab samale objektile, millele $box1 juba viitab, muudad ühte ja sama objekti,mõlemad muutujad näevad muutust
+}
+
+Box::$count = 1;
+Box::$count = 2; 
+var_dump(Box::$count, Box::$count);
+Box::getCount();
+var_dump(Box::class);
+MetalBox::getCount();
 $box1 = new Box();
-$box1->num = 1;
-$box2 = $box1;
-$box2 = clone $box1; // kui ei taha, et oleks viide, vaid tõesti koopia
-$box2->num = 2; 
-var_dump($box1->num, $box2->num);
-var_dump($box1,$box2);
+$box1->getWidth();
 
-// PHP-s on võimalik töötada nii väärtuste kui viidetega, ja need käituvad väga erinevalt.
-$array = [1,2,3,4,5];
-
-for($i=0;$i<count($array);$i++){ 
-    $num = &$array[$i]; // See muutuja EI OMA väärtust, vaid VIITAB väärtusele.
-    $num *=2;
-}
-
-foreach($array as &$num) {
-    $num *=2;
-    var_dump($num);
-}
-var_dump($array);
